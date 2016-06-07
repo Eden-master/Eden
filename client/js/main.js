50ff4db04fb0007a93ced9198ea14f44e65ddbfc
jeyip <jeremy.yip7@gmail.com>
2016-06-07 10:27:24 -0700

    updated some comments

--- client/js/main.js
+++ client/js/main.js
@@ -6,7 +6,6 @@ class Main extends React.Component {
   render() {
     return (
       <div>
-        <h1>Eden</h1>
         <div className='container'>
           <ChatboxContainer url="http://localhost:3000"/>
         </div>
