109c69aa40ed890664a341bc5397673e7e668d8a
Michael Laythe <mrlaythe24@aol.com>
2016-06-06 22:53:10 -0700

    added input box to listen for msg

--- client/js/containers/ChatboxContainer.js
+++ client/js/containers/ChatboxContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Chatbox from '../components/Chatbox';
 import request from 'browser-request';
+import Chatbox from '../components/Chatbox';
+import SubmitMsg from '../components/SubmitMsg';
 
 class ChatboxContainer extends React.Component {
   constructor() {
@@ -18,18 +19,37 @@ class ChatboxContainer extends React.Component {
   }
 
   getData() {
-    request(
-      this.props.url + '/messages?branch_id=' + this.state.bannerID, (err, res, body) => {
+    request(this.props.url + '/messages?branch_id=' + this.state.bannerID, (err, res, body) => {
         this.setState({
           messages: JSON.parse(body)
         });
+    });
+  }
+
+  handleKeyPress(e) {
+    if (e.keyCode === 13) {
+      console.log(this.state.bannerID);
+      let objToSend = JSON.stringify({
+        username: 'werollin',
+        message: e.target.value,
+        branch_id: this.state.bannerID
       });
+
+      request({method:'POST', url: this.props.url + '/messages?branch_id=' + this.state.bannerID, body: objToSend, json:true}, on_response);
+
+      const on_response = (err, res, body) => {
+        if (err) throw new Error(err);
+
+        console.log('response', response);
+      };
+    }
   }
 
   render() {
     return (
       <div>
         <Chatbox messages={this.state.messages}/>
+        <SubmitMsg handleClick={this.handleKeyPress} />
       </div>
     );
   }
