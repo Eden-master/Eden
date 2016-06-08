ccba6ae1705741def2bb9f9ab35ab78358137af2
anwaukoni <anwaukoni@gmail.com>
2016-06-08 12:43:13 -0700

    oauth merged with the rest

--- server/server.js
+++ server/server.js
@@ -8,11 +8,8 @@ const bodyParser = require('body-parser');
 const  cookieParser = require('cookie-parser');
 // const data = require('./database/controller/dbController');
 const messageController = require('./messages/messageController');
-<<<<<<< HEAD
 const authentication = require('./oauthTwo.js');
-=======
 const branchController = require('./branches/branchController')
->>>>>>> 067ea97a6b5197b0826b7c6e8861219a2f6cbd49
 
 ///////////////////////////////////////////
 //Middelware:
@@ -23,15 +20,13 @@ app.use(bodyParser.json());
 
 app.get('/oauth2', authentication.getAuthCode);
 app.get('/oauth2callback', authentication.getTokenCode);
+
 app.get('/messages', messageController.getMessages);
 app.post('/messages', messageController.postMessages);
 
-<<<<<<< HEAD
-=======
 app.post('/branch', branchController.createBranch, messageController.getMessages);
 app.post('/back', branchController.getPreviousBranch, messageController.getMessages);
 app.get('/everything', branchController.getAllTheBranches);
->>>>>>> 067ea97a6b5197b0826b7c6e8861219a2f6cbd49
 
 app.listen(3000, () => {
 	console.log('listening on port 3000');
