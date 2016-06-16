d2ccbfdd0428ec0db59f129e4271a869d9452f52
Michael Laythe <mrlaythe24@aol.com>
2016-06-15 23:03:02 -0700

    setup testing for react with jsdom and enzyme

--- test/react_test.js
+++ test/react_test.js
@@ -1,1 +1,12 @@
 "use strict";
+const React = require('react');
+const { expect } = require('chai');
+const { shallow, mount, render } = require('enzyme');
+const ChatboxContainer = require('../client/js/containers/ChatboxContainer');
+
+describe('<ChatboxContainer/>', function() {
+  it ('should have three children', function() {
+    const wrapper = shallow(<ChatboxContainer/>);
+    expect(wrapper.children().length).to.equal(3);
+  });
+});
